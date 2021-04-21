63abb8cf39cdb89de018dccce373209fd3b26aef
Alessandro Moura <alessandromoura@hotmail.com>
2021-04-22 09:47:55 +1200

    Adding more tests

--- BuyTicketCorinthians.Tests/GoodRequests.cs
+++ BuyTicketCorinthians.Tests/GoodRequests.cs
@@ -33,7 +33,46 @@ namespace BuyTicketCorinthians.Tests
                 price = 150
             };
 
+            // Call Logic App
+            var result = await CallLogicApp<Models.APIRequest>(request, "any");
 
+            // Receive Logic App response
+            var response = await GetHttpResponseMessage<Models.LogicAppsErrorResponse>(result);
+
+            // Verify that the http code returned is 202 (Accepted)
+            string details = response.error != null ? response.error.message : string.Empty;
+            Assert.IsTrue(result.StatusCode == System.Net.HttpStatusCode.Accepted, 
+                $"Expected '{System.Net.HttpStatusCode.Accepted}' but received '{result.StatusCode}. Details: {details}'");
+
+            // Obtain LogicApps RunId to investigate its actions
+            var runId = result.Headers.GetValues("x-ms-workflow-run-id").First();
+
+            while (true)
+            {
+                // Get Logic App execution status
+                var workflowStatus = await LogicAppClient.WorkflowRuns.GetAsync(ResourceGroupName, LogicAppName, runId);
+
+                // Await workflow completion to retrieve the action result
+                if (workflowStatus.Status == WorkflowStatus.Succeeded || workflowStatus.Status == WorkflowStatus.Failed)
+                {
+                    // Get HTTP call status
+                    var httpAction = await LogicAppClient.WorkflowRunActions.GetAsync(ResourceGroupName, LogicAppName, runId, "Buy_Ticket");
+                    Assert.IsTrue(httpAction.Code == "OK", "Response not expected!", 
+                        $"Expected 'OK' but received '{httpAction.Code}'");
+
+                    var httpActionResponse = await LogicAppClient.HttpClient.GetAsync(httpAction.OutputsLink.Uri);
+                    var httpActionResponseContent = await GetHttpResponseMessage<Models.APIResponse>(httpActionResponse, true);
+                    Assert.IsTrue(httpActionResponseContent.mensagem == "Vai Corinthians!!!");
+
+                    // Get Log Storage account
+                    var storageAction = await LogicAppClient.WorkflowRunActions.GetAsync(ResourceGroupName, LogicAppName, runId, "Log_Buy_Ticket_Success");
+                    Assert.IsTrue(storageAction.Code == "Created", "Response not expected!",
+                        $"Expected 'Created' but received '{storageAction.Code}'");
+
+                    break;
+                }
+                Thread.Sleep(500);
+            }
         }
 
     }
